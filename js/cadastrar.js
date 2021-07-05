ac6ebce55df27b65f5e77933fd9d1d1b87e38e3d
hellem.moura79@gmail.com <hellem.moura79@gmail.com>
2021-07-04 21:44:02 -0300

    modificações

---
+++ js/cadastrar.js
@@ -0,0 +1,23 @@
+function validarInputs(event) {
+    event.preventDefault();
+    var userName = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var passConfirmation = document.getElementById('passConfirmation');
+
+    if (userName.length >= 6) {
+        if (password.length >= 8) {
+            if (password.length == passConfirmation.length) {
+                alert("Cadastro efetuado!");
+            }
+            else{
+                alert("As senhas não condizem!!!");
+            }
+        }
+        else{
+            alert("A senha precisa ter no mínimo 8 caracteres");
+        }
+    }
+    else{
+        alert("Seu nome de usuário precisa ter no mínimo 6 caracteres");
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,24 +1,37 @@
-    var senha = document.getElementById('password');
-    if(senha.type == 'password'){
-        senha.type = 'text';
-    }
-    else{
-        senha.type = 'password';
-    }
+var senha = documento.getElementById('senha');
+if (senha.type == 'senha') {
+    senha.type = 'texto';
+}
+else {
+    senha.type = 'senha';
+}
+function verificar(event) {
+    event.preventDefault();
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
 
-    var email = document.getElementById('email');
-    var password = document.getElementById('passoword');
+    let verificarEmail = "projetoweb@gmail.com";
+    let verificarSenha = "projetoweb";
 
-    email.addEventListener('focus',()=>{
-        email.style.borderColor = "#4A5F6A"
-    });
-    email.addEventListener('blur',()=>{
-        email.style.borderColor = "#ccc"
-    });
-    password.addEventListener('focus',()=>{
-        password.style.borderColor = "#4A5F6A"
-    });
-    password.addEventListener('blur',()=>{
-        password.style.borderColor = "#ccc"
-    });    
+    if (email == verificarEmail && password == verificarSenha) {
+        alert("Success");
+    }
+    else {
+        alert("Login incorreto");
+    }
+}
+
+
+/*email.addEventListener('focus',()=>{
+    email.style.borderColor = "#4A5F6A"
+});
+email.addEventListener('blur',()=>{
+    email.style.borderColor = "#ccc"
+});
+password.addEventListener('focus',()=>{
+    password.style.borderColor = "#4A5F6A"
+});
+password.addEventListener('blur',()=>{
+    password.style.borderColor = "#ccc"
+});   */
 
